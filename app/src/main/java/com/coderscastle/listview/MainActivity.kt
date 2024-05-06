ca2d231542fcd1345857e6a262422e37deb7e959
limonreza00 <limonreza3.1416@gmail.com>
2024-05-06 16:56:57 +0600

    Simple listview works

---
+++ app/src/main/java/com/coderscastle/listview/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.coderscastle.listview
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val listView = findViewById<ListView>(R.id.listView)
+
+        val array = arrayOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20")
+        val adapter = ArrayAdapter(this, R.layout.list_item_layout, array)
+        listView.adapter = adapter
+    }
+}
